c012d73bf2dd51d854fd088c9ea7df4690e6446d
marbenjaminkatamba-sdc <mark.katamba@sportdataco.com>
2026-07-28 12:22:08 +0300

    Add MongoDB instrumentation and tracer spans
    Register MongoDB instrumentation and add tracing support. _opentelemetry-js: add @opentelemetry/instrumentation-mongodb, handle Resource API compatibility, register MongoDBInstrumentation and disable the auto-instrumentation entry for mongodb. node-service-blue/routes: import OpenTelemetry API, create a tracer from package.json and wrap the fibonacci work in an active span. Update package.json and package-lock.json to include the new dependency and lockfile changes.

--- _opentelemetry-js/index.js
+++ _opentelemetry-js/index.js
@@ -7,23 +7,36 @@ const {
 const {
   getNodeAutoInstrumentations
 } = require("@opentelemetry/auto-instrumentations-node");
+const {
+  MongoDBInstrumentation
+} = require("@opentelemetry/instrumentation-mongodb");
 
-const { Resource } = require("@opentelemetry/resources");
+const otelResources = require("@opentelemetry/resources");
 const {
   SEMRESATTRS_SERVICE_NAME,
   SEMRESATTRS_SERVICE_VERSION
 } = require("@opentelemetry/semantic-conventions");
 
 module.exports = (serviceName, serviceVersion) => {
+  const resource =
+    typeof otelResources.resourceFromAttributes === "function"
+      ? otelResources.resourceFromAttributes({
+          [SEMRESATTRS_SERVICE_NAME]: serviceName,
+          [SEMRESATTRS_SERVICE_VERSION]: serviceVersion
+        })
+      : new otelResources.Resource({
+          [SEMRESATTRS_SERVICE_NAME]: serviceName,
+          [SEMRESATTRS_SERVICE_VERSION]: serviceVersion
+        });
+
   const sdk = new NodeSDK({
-    resource: new Resource({
-      [SEMRESATTRS_SERVICE_NAME]: serviceName,
-      [SEMRESATTRS_SERVICE_VERSION]: serviceVersion
-    }),
+    resource,
     traceExporter: new OTLPTraceExporter(),
     instrumentations: [
+      new MongoDBInstrumentation(),
       getNodeAutoInstrumentations({
-        "@opentelemetry/instrumentation-fs": { enabled: false }
+        "@opentelemetry/instrumentation-fs": { enabled: false },
+        "@opentelemetry/instrumentation-mongodb": { enabled: false }
       })
     ]
   });
